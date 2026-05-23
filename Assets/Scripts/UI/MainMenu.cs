abe4d53d8fdeefe86057b870d003ea44b4950d0b
ная <nastya034rus@yandex.ru>
2026-05-23 20:02:08 +0300

    в скрипт главного меню добавлены методы для кнопок, в скрипт настроек добавлена возможность вернуться на предыдущую сцену

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,4 +1,20 @@
-public class MainMenu
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
 {
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("2");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
     
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -1,4 +1,20 @@
-public class Settings
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
 {
+    public static int _previousSceneIndex = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToPreviousScene();
+        }
+    }
     
+    public void ReturnToPreviousScene()
+    {
+        SceneManager.LoadScene(_previousSceneIndex);
+    }
 }
