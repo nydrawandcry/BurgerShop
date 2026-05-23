97b7ddd155b78cfcbac48ca920a395542ad62572
ная <nastya034rus@yandex.ru>
2026-05-23 21:37:24 +0300

    добавлен переход на предыдущую сцену при выборе уровня

--- Assets/Scripts/UI/LevelChoosing.cs
+++ Assets/Scripts/UI/LevelChoosing.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class LevelChoosing : MonoBehaviour
 {
+    public static int _previousSceneIndex = 0;
+    
+    private void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            ReturnToPreviousScene();
+        }
+    }
+    
     public void StartLevel1()
     {
         SceneManager.LoadSceneAsync(3);
@@ -16,5 +27,10 @@ public class LevelChoosing : MonoBehaviour
     public void StartLevel3()
     {
         SceneManager.LoadSceneAsync(5);
+    }
+    
+    public void ReturnToPreviousScene()
+    {
+        SceneManager.LoadScene(_previousSceneIndex);
     }
 }
